4699aaaf11352a95b73336169da2d11b748d8cdd
Zero <zero1729@protonmail.com>
2019-10-01 18:01:09 +0100

    Fleshed Basic Audio Player ⚙
    - Wavesurfur loads local files through `fs`, using the `.loadArrayBuffer` method

---
+++ src/renderer/utils/player.js
@@ -0,0 +1,99 @@
+const ws = require('wavesurfer.js')
+const fs = require('fs')
+
+
+export default class Player {
+    constructor(track, props) {
+        this.currentTrack = track
+        this.autoPlays = props.autoplay
+        this.oldPool = props.pool // For shuffle/unshuffle
+
+        this.device = new ws.create({
+            container: "#waveform",
+            fillParent: true,
+            scrollParent: false,
+            waveColor: "#E8E8E8",
+            progressColor: "#B9B9B9",
+            cursorColor: "#B9B9B9",
+            pixelRatio    : props.window.devicePixelRatio,
+            minPxPerSec   : 20,
+            reflection: true,
+            height: 25,
+            cursorWidth: 1,
+            cursorHeight: 0.8,
+            hideScrollbar : false,
+            volume: props.volume,
+            loop: props.loop,
+            mute: props.mute,
+            audioRate     : 1,
+            loopSelection: false,
+            normalize     : false,
+            audioContext  : null,
+            dragSelection : true,
+            loopSelection : true,
+            interact      : true,
+            autoCenter    : true
+        })
+    }
+
+    clear() {
+        this.device.pause()
+        this.device.empty()
+    }
+
+    updateVolume(val) {
+        this.device.setVolume(val)
+    }
+
+    formatTime(time) {
+        // Lets obtain our min and secs
+        let min = parseInt((time / 60) % 60)
+        let secs = parseInt(time % 60)
+
+        // Format min and secs
+        let m = min < 10 ? "0"+min : min
+        let s = secs < 10 ? "0"+secs : secs
+
+        // return parsed value
+        return `${m}:${s}` || '00:00'
+    }
+
+    getDuration() {
+        return this.formatTime(this.device.getDuration())
+    }
+
+    getCurrentPos() {
+        return this.formatTime(this.device.getCurrentTime())
+    }
+
+    playNew(path) {
+        this.device.stop()
+        this.device.empty()
+
+        fs.readFile(path, (err, buffer) => {
+            this.device.loadArrayBuffer(buffer.buffer)
+        })
+    }
+
+    play() {
+        this.device.play()
+    }
+
+    playpause() {
+        this.device.playPause()
+    }
+
+    // Audio actions
+    mute() {
+        this.device.toggleMute()
+    }
+
+    shuffle() {}
+
+    unShuffle() {}
+
+    // Looping is handled in 'audioprocess';
+    // so if loop single on, when audio is done we reload the same track
+    // When it is loopAll, we check the track's current indx in the pool, if it is last, we play first,
+    // if not we just leave the track finished, reset current Track
+}
