49847365f5346ca85a2e2071415f7bbc40e0655d
Zero <zero1729@protonmail.com>
2020-02-27 20:37:55 +0100

    File check handles inexistent files
    Maybe we split the check into 'exists' and 'isFile' in cases where even the folder is inexistent.

--- src/renderer/utils/file.js
+++ src/renderer/utils/file.js
@@ -3,7 +3,12 @@ const fs = require('fs')
 
 const isFile = (path) => {
     // Little fn to check whether a path is a file or not
-    return fs.statSync(path).isFile()
+    // In case it does not exist
+    try {
+       return fs.statSync(path).isFile()
+    } catch (e) {
+        return false
+    }
 }
 
 module.exports = { isFile }
